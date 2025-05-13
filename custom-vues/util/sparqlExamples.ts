03cf765c509715158ad1a84d344d4f921bfabbe9
KoalaGeo <edlew@bgs.ac.uk>
2025-05-13 10:21:35 +0100

    Update sparqlExamples.ts
    Fix typo

--- custom-vues/util/sparqlExamples.ts
+++ custom-vues/util/sparqlExamples.ts
@@ -1,5 +1,40 @@
 export default [
    {
+        title: "Example Select Query",
+        shortTitle: "Basic Select",
+        description: "This is a basic example of a select query, which lists the first 10 triples.",
+        query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT *
+WHERE {
+    ?s ?p ?o .
+} LIMIT 10`
+    },
+    {
+        title: "Example Construct Query",
+        shortTitle: "Basic Construct",
+        description: "This is a basic example of a construct query, which constructs a graph of the first 10 triples.",
+        query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+CONSTRUCT {
+    ?s ?p ?o .
+}
+WHERE {
+    ?s ?p ?o .
+} LIMIT 10`
+    },
+    {
+        title: "Vocabularies - Counting Concepts",
+        shortTitle: "Concept Count",
+        description: "Here is an example query for VocPrez you can copy 'n paste into the Query UI text area above to test with. It counts the number of vocabulary Concepts in the all vocabularies in this system and will return an integer: ",
+        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT (COUNT(?c) AS ?count)
+WHERE {
+    ?c a skos:Concept .
+}`
+    },
+   {
         title: "Get all triples for a known “code” (skos:notation)",
         shortTitle: "Get all triples for a known “code” (skos:notation)",
         description: "This is a BGS example of a select query, which lists the first 10 triples.",
@@ -81,7 +116,7 @@ SELECT *
 WHERE {
 ?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
 FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))v
+FILTER (?p IN (skos:notation))
 FILTER regex(ucase(str(?label)), "SAN")
 FILTER (?maxAge < 66)
 } ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
@@ -231,55 +266,5 @@ FILTER (?p IN (skos:notation))
 FILTER (?minAge >= 433.4)
 FILTER (?maxAge <= 443.8)
 } ORDER BY ?minAge OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Example Select Query",
-        shortTitle: "Basic Select",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
-        query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT *
-WHERE {
-    ?s ?p ?o .
-} LIMIT 10`
-    },
-    {
-        title: "Example Construct Query",
-        shortTitle: "Basic Construct",
-        description: "This is a basic example of a construct query, which constructs a graph of the first 10 triples.",
-        query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-CONSTRUCT {
-    ?s ?p ?o .
-}
-WHERE {
-    ?s ?p ?o .
-} LIMIT 10`
-    },
-    {
-        title: "Vocabularies - Counting Concepts",
-        shortTitle: "Concept Count",
-        description: "Here is an example query for VocPrez you can copy 'n paste into the Query UI text area above to test with. It counts the number of vocabulary Concepts in the all vocabularies in this system and will return an integer: ",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-
-SELECT (COUNT(?c) AS ?count)
-WHERE {
-    ?c a skos:Concept .
-}`
-    },
-    {
-        title: "Spatial Data Catalog - Basic Feature Information",
-        shortTitle: "Feature Info",
-        description: "Here is an example query for SpacePrez for use in the Query UI above. It returns most two Features' URIs and their titles.",
-        query: `PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-SELECT ?uri ?title
-WHERE {
-    ?uri a geo:Feature ;
-        dcterms:title|rdfs:label ?title .
-}
-LIMIT 2`
-    },
+    }
 ];
