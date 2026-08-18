24b25d09a4a2edee5d8acf4c4c510ca2e7a6cbab
Rachel Heaven <reh@bgs.ac.uk>
2026-08-19 00:42:25 +0100

    chore: added recursive SPARQL query example and shortened the list so page is less wide

--- custom-vues/util/sparqlExamples.ts
+++ custom-vues/util/sparqlExamples.ts
@@ -35,35 +35,21 @@ WHERE {
 }`
     },
    {
-        title: "Get all triples for a known “code” (skos:notation)",
-        shortTitle: "Get all triples for a known “code” (skos:notation)",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
+        title: "Get all triples for a given Lexicon code",
+        shortTitle: "Get all triples for a given Lexicon code",
+        description: "Get all triples for a given Lexicon code",
         query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 SELECT *
 WHERE {
 ?s ?p ?o; skos:notation ?code .
-FILTER regex(str(?s), "Geochronology/Division")
-FILTER (ucase(str(?code)) = "J")
+FILTER regex(str(?s), "Lexicon/NamedRockUnit")
+FILTER (ucase(str(?code)) = "MMG")
 }`
     }, 
     {
-        title: "top-level Geochron for Lexicon search “maxAge” dropdown",
-        shortTitle: "top-level Geochron for Lexicon search “maxAge” dropdown",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX gc: <http://data.bgs.ac.uk/ref/Geochronology/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; gc:minAgeValue ?minAge; gc:maxAgeValue ?maxAge.
-FILTER regex(str(?s), "Geochronology/Division")
-FILTER (?p IN (skos:notation))
-FILTER (STRENDS(ucase(str(?label)), "EON") || STRENDS(ucase(str(?label)), "ERA") || STRENDS(ucase(str(?label)), "PERIOD") || STRENDS(ucase(str(?label)), "EPOCH"))
-} ORDER BY ?minAge OFFSET 0 LIMIT 100`
-    }, 
-    {
-        title: "Lexicon/NamedRockUnit - AGE ONLY",
-        shortTitle: "Lexicon/NamedRockUnit - AGE ONLY",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
+        title: "Lexicon/NamedRockUnit within age range sorted by age",
+        shortTitle: "Lexicon/NamedRockUnit within age range sorted by age",
+        description: "Lexicon/NamedRockUnit within age range sorted by age",
         query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX pld: <http://purl.org/linked-data/registry#>
 PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
@@ -72,150 +58,14 @@ WHERE {
 ?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
 FILTER regex(str(?s), "Lexicon/NamedRockUnit")
 FILTER (?p IN (skos:notation))
-FILTER (?maxAge < 66)
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-   {
-        title: "Lexicon/NamedRockUnit - CODE ONLY",
-        shortTitle: "Lexicon/NamedRockUnit - CODE ONLY",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX pld: <http://purl.org/linked-data/registry#>
-PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
-FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?code)), "SAND")
-} ORDER BY (ucase(?code)) OFFSET 0 LIMIT 100`
-    },
-   {
-        title: "Lexicon/NamedRockUnit - DESC ONLY",
-        shortTitle: "Lexicon/NamedRockUnit - DESC ONLY",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX pld: <http://purl.org/linked-data/registry#>
-PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
-FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?label)), "SAND")
+FILTER (?minAge >= 433.4)
+FILTER (?maxAge <= 443.8)
 } ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
     },
     {
-        title: "Lexicon/NamedRockUnit - AGE + CODE",
-        shortTitle: "Lexicon/NamedRockUnit - AGE + CODE",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX pld: <http://purl.org/linked-data/registry#>
-PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
-FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?label)), "SAN")
-FILTER (?maxAge < 66)
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Lexicon/NamedRockUnit - AGE + DESC",
-        shortTitle: "Lexicon/NamedRockUnit - AGE + DESC",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX pld: <http://purl.org/linked-data/registry#>
-PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
-FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?label)), "SANDST")
-FILTER (?maxAge < 66)
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Lexicon/NamedRockUnit - CODE + DESC",
-        shortTitle: "Lexicon/NamedRockUnit - CODE + DESC",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX pld: <http://purl.org/linked-data/registry#>
-PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
-FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?code)), "SAND")
-FILTER regex(ucase(str(?label)), "SANDST")
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Lexicon/NamedRockUnit - CODE + DESC + AGE",
-        shortTitle: "Lexicon/NamedRockUnit - CODE + DESC + AGE",
-        description: "This is a BGS example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX pld: <http://purl.org/linked-data/registry#>
-PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; skos:definition ?defn; pld:status ?status; lex:hasYoungestAgeValue ?minAge; lex:hasOldestAgeValue ?maxAge .
-FILTER regex(str(?s), "Lexicon/NamedRockUnit")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?label)), "SANDS")
-FILTER (?maxAge < 66)
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Geochronology/Division - DESC ONLY",
-        shortTitle: "Geochronology/Division - DESC ONLY",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX gc: <http://data.bgs.ac.uk/ref/Geochronology/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; gc:minAgeValue ?minAge; gc:maxAgeValue ?maxAge.
-FILTER regex(str(?s), "Geochronology/Division")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?label)), "JUR")
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Geochronology/Division - CODE ONLY",
-        shortTitle: "Geochronology/Division - CODE ONLY",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX gc: <http://data.bgs.ac.uk/ref/Geochronology/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; gc:minAgeValue ?minAge; gc:maxAgeValue ?maxAge.
-FILTER regex(str(?s), "Geochronology/Division")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?code)), "J")
-} ORDER BY (ucase(?code)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Geochronology/Division - DESC + CODE",
-        shortTitle: "Geochronology/Division - DESC + CODE",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX gc: <http://data.bgs.ac.uk/ref/Geochronology/>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; gc:minAgeValue ?minAge; gc:maxAgeValue ?maxAge.
-FILTER regex(str(?s), "Geochronology/Division")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?code)), "J")
-FILTER regex(ucase(str(?label)), "JUR")
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "EarthMaterialClass/RockClassAll - DESC ONLY",
-        shortTitle: "EarthMaterialClass/RockClassAll - DESC ONLY",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
+        title: "EarthMaterialClass/RockClassAll where label contains SAND, first 100 matches",
+        shortTitle: "EarthMaterialClass/RockClassAll where label contains SAND",
+        description: "EarthMaterialClass/RockClassAll where label contains SAND",
         query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 SELECT *
 WHERE {
@@ -226,36 +76,9 @@ FILTER regex(ucase(str(?label)), "SAND")
 } ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
     },
     {
-        title: "EarthMaterialClass/RockClassAll - CODE ONLY",
-        shortTitle: "EarthMaterialClass/RockClassAll - CODE ONLY",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; .
-FILTER regex(str(?s), "EarthMaterialClass/RockClassAll")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?code)), "S")
-} ORDER BY (ucase(?code)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "EarthMaterialClass/RockClassAll - DESC + CODE",
-        shortTitle: "EarthMaterialClass/RockClassAll - DESC + CODE",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
-        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-SELECT *
-WHERE {
-?s ?p ?o; skos:prefLabel ?label; skos:notation ?code; .
-FILTER regex(str(?s), "EarthMaterialClass/RockClassAll")
-FILTER (?p IN (skos:notation))
-FILTER regex(ucase(str(?code)), "S")
-FILTER regex(ucase(str(?label)), "CARBON")
-} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
-    },
-    {
-        title: "Geochronology/Division overlapping Lexicon “minAge/maxAge” e.g. Lexicon ABF “Abbotsford Flags” (433.4 to 443.8)",
-        shortTitle: "Geochronology/Division overlapping Lexicon “minAge/maxAge” e.g. Lexicon ABF “Abbotsford Flags” (433.4 to 443.8)",
-        description: "This is a basic example of a select query, which lists the first 10 triples.",
+        title: "Geochronology/Division within age range sorted by age",
+        shortTitle: "Geochronology/Division within age range sorted by age",
+        description: "Geochronology/Division within age range sorted by age",
         query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX gc: <http://data.bgs.ac.uk/ref/Geochronology/>
 SELECT *
@@ -265,6 +88,33 @@ FILTER regex(str(?s), "Geochronology/Division")
 FILTER (?p IN (skos:notation))
 FILTER (?minAge >= 433.4)
 FILTER (?maxAge <= 443.8)
-} ORDER BY ?minAge OFFSET 0 LIMIT 100`
+} ORDER BY ?minAge`
+    },
+    {
+        title: "Geoscience Thesaurus concepts matching SAND",
+        shortTitle: "Geoscience Thesaurus concepts matching SAND",
+        description: "Geoscience Thesaurus concepts matching SAND",
+        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX pld: <http://purl.org/linked-data/registry#>
+PREFIX lex: <http://data.bgs.ac.uk/ref/Lexicon/>
+SELECT *
+WHERE {
+?s ?p ?o; skos:prefLabel ?label .
+FILTER regex(str(?s), "GeoscienceThesaurus/Concept")
+FILTER (?p IN (skos:prefLabel))
+FILTER regex(ucase(str(?label)), "SAND")
+} ORDER BY (ucase(?label)) OFFSET 0 LIMIT 100`
+    },
+	{
+        title: "Geochronology Divisions within the Quaternary (Q)",
+        shortTitle: "Geochronology Divisions within the Quaternary (Q)",
+        description: "Geochronology Divisions within the Quaternary (Q)",
+        query: `PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+    SELECT DISTINCT ?narrower ?label ?notation
+    WHERE {{
+        <http://data.bgs.ac.uk/id/Geochronology/Division/Q> skos:narrower+ ?narrower .
+        OPTIONAL {{ ?narrower skos:prefLabel ?label }}
+        OPTIONAL {{ ?narrower skos:notation ?notation }}`
     }
 ];
